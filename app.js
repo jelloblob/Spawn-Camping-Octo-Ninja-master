7a2019af5707eca0bae84f9a476f7e57e82a2ce5
jelloblob <ian@saturnia.org>
2015-10-20 10:37:25 -1000

    Diamond mine added

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var score = 0; 
 
+var diamondminePoint = 350;
+
 var silverPoint = 10;
 
 var BetterMiners
 
-var captainshieldPiont = 200;
+var goldminePoint = 200;
 
 var goldPoint = 25;
 
@@ -39,42 +41,46 @@ function timer()
 }
 $("#zane").click(function(){
 //everything below this is for the text that pops up and how the values work
-	computerChoice = Math.floor(Math.random()*115);
+	computerChoice = Math.floor(Math.random()*125);
 	if(computerChoice < 5){
 		computerChoice = "Diamond";
 			$('#received').text("You got Diamond");
-			
 			score = diamondPoint + score;
-			$('#Score').text('Your Score ' + score);
+			$('#Score').text('You have $' + score);
 	}
 	else if(computerChoice > 55){
 		computerChoice = "Nothing";	
 			$('#received').text("You got Rocks");
 			score = nothingPoint + score;	
-			$('#Score').text('Your Score ' + score);
+			$('#Score').text('You have $' + score);
 	}	
 	else if(computerChoice > 5 && computerChoice < 16){
 		computerChoice = "Gold";
 			$('#received').text("You got Gold");
 			score = goldPoint + score;
-			$('#Score').text('Your Score ' + score);
+			$('#Score').text('You have $' + score);
 	}
 	else if(computerChoice > 16 && computerChoice < 31){
 		computerChoice = "Silver";
 			$('#received').text("You got Silver");	
 			score = silverPoint + score;
-			$('#Score').text('Your Score ' + score);
+			$('#Score').text('You have $' + score);
 	}
 	else if(computerChoice > 31 && computerChoice <33 ){
-		computerChoice = "Shield";
-			alert("You got The Shield");	
-			score = captainshieldPiont + score;
+		computerChoice = "Diamond Mine";
+			alert("You Found a Diamond Mine");	
+			score = diamondminePoint + score;
 	}
-	else if(computerChoice > 33 && computerChoice < 55){
+	else if(computerChoice > 33 && computerChoice <35 ){
+		computerChoice = "Gold Mine";
+			alert("You found a Gold Mine");	
+			score = goldminePoint + score;
+	}
+	else if(computerChoice > 35 && computerChoice < 55){
 		computerChoice = "Refined Rocks";
 			$('#received').text("You got Refined Rocks");
 			score = refinedPoint + score;
-			$('#Score').text('Your Score ' + score);
+			$('#Score').text('You have $' + score);
 	}
 });
 
